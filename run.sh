46628d320231c663b718c3b43a5eb39e979cb361
Chip Wolf ‮ <hello@chipwolf.uk>
2019-08-22 15:23:25 +0100

    :poop: test load balancing

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ if [ -f /mnt/velocity.toml ]; then
 	curl $VELOCITY_TOML_URL > /velocity/velocity.toml
 fi
 
+sed -i "s/A Velocity Server/${HOSTNAME} - $(date +%s | sha256sum | base64 | head -c 8)/" /velocity/velocity.toml
+
 COMMAND="java -Xmx$JAVA_MEMORY -Xms$JAVA_MEMORY $JAVA_ARGS -jar velocity.jar"
 echo "Running Velocity - $COMMAND"
 cd /velocity 
